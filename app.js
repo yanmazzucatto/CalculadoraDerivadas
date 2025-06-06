d7d746930940ea3edcae33a8343be8edc65ae308
Matheus Santana <143432265+mathqwe@users.noreply.github.com>
2025-06-06 00:12:35 +0000

    corrige porta para produção

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public/index.html"));
 })
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Servidor rodando em http://localhost:${PORT}`);
-})
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
+
